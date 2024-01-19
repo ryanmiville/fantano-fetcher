bcd9093fbc8097fd579b59b44e85555460d03823
Ryan Miville <ryan.miville@terminus.com>
2024-01-19 17:32:11 -0500

    Add dependencies for drizzle integration and introspection

---
+++ db/schema.ts
@@ -0,0 +1,36 @@
+import {
+  date,
+  int,
+  mysqlTable,
+  primaryKey,
+  text,
+  tinyint,
+  unique,
+  varchar,
+} from "drizzle-orm/mysql-core";
+
+export const reviews = mysqlTable(
+  "reviews",
+  {
+    id: int("id").autoincrement().notNull(),
+    videoId: varchar("video_id", { length: 255 }).notNull(),
+    title: varchar("title", { length: 255 }).notNull(),
+    artist: varchar("artist", { length: 255 }).notNull(),
+    album: varchar("album", { length: 255 }).notNull(),
+    description: text("description").notNull(),
+    thumbnailUrl: varchar("thumbnail_url", { length: 255 }).notNull(),
+    // you can use { mode: 'date' }, if you want to have Date as type for this column
+    publishDate: date("publish_date", { mode: "string" }).notNull(),
+    watchUrl: varchar("watch_url", { length: 255 }).notNull(),
+    yellowFlannel: tinyint("yellow_flannel"),
+    rating: int("rating"),
+  },
+  (table) => {
+    return {
+      reviewsId: primaryKey({ columns: [table.id], name: "reviews_id" }),
+      videoId: unique("video_id").on(table.videoId),
+    };
+  }
+);
+
+export type Review = typeof reviews.$inferSelect;
