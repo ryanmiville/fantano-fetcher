2b34ff33f2c41927c3c000e3ba2941aea58062c6
Ryan Miville <ryan.miville@terminus.com>
2024-01-20 15:18:06 -0500

    Refactor ReviewsTable component and add data fetching functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
-import ReviewsTable from "@/components/ReviewsTable";
+import { Review, ReviewsTable } from "@/components/ReviewsTable";
 
-export default function Home() {
+import { db, reviews } from "@/db";
+
+async function selectReviews() {
+  return (await db
+    .select({
+      artist: reviews.artist,
+      album: reviews.album,
+      rating: reviews.rating,
+      publishDate: reviews.publishDate,
+    })
+    .from(reviews)
+    .limit(25)) as Review[];
+}
+
+export default async function Home() {
+  const data = await selectReviews();
   return (
     <main className="min-h-screen p-24">
       <div className="flex flex-col items-center text-6xl lg:text-8xl font-extrabold pb-4">
@@ -8,7 +23,7 @@ export default function Home() {
         <h1>Fetcher</h1>
       </div>
       <section className="max-w-screen-xl mx-auto">
-        <ReviewsTable />
+        <ReviewsTable reviews={data} />
       </section>
     </main>
   );
--- components/ReviewsTable.tsx
+++ components/ReviewsTable.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
@@ -6,19 +17,20 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { db, reviews } from "@/db";
-
-async function selectReviews() {
-  return (await db
-    .select({
-      artist: reviews.artist,
-      album: reviews.album,
-      rating: reviews.rating,
-      publishDate: reviews.publishDate,
-    })
-    .from(reviews)
-    .limit(10)) as Review[];
-}
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import * as React from "react";
 
 export type Review = {
   artist: string;
@@ -27,28 +39,168 @@ export type Review = {
   publishDate: string;
 };
 
-export default async function ReviewsTable() {
-  const rows = await selectReviews();
+const columns: ColumnDef<Review>[] = [
+  {
+    accessorKey: "artist",
+    header: "Artist",
+  },
+  {
+    accessorKey: "album",
+    header: "Album",
+  },
+  {
+    accessorKey: "publishDate",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Publish Date
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "rating",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Rating
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const review = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem>Watch Review</DropdownMenuItem>
+            <DropdownMenuItem>Open in Spotify</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
+
+export type Props = {
+  reviews: Review[];
+};
+
+export function ReviewsTable({ reviews }: Props) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+
+  const table = useReactTable({
+    data: reviews,
+    columns: columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+    },
+  });
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Artist</TableHead>
-          <TableHead>Album</TableHead>
-          <TableHead>Publish Date</TableHead>
-          <TableHead>Rating</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {rows.map((review) => (
-          <TableRow key={review.album}>
-            <TableCell>{review.artist}</TableCell>
-            <TableCell>{review.album}</TableCell>
-            <TableCell>{review.publishDate}</TableCell>
-            <TableCell>{review.rating}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <div className="w-full">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter artists..."
+          value={(table.getColumn("artist")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("artist")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
