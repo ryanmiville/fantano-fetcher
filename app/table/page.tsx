f390a250bafdf2688e10e86c3454587aa34cf153
Ryan Miville <ryan.miville@terminus.com>
2024-01-22 21:57:09 -0500

    add tasks example

--- app/table/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DataTableDemo } from "./data-table";
-
-export default async function DemoPage() {
-  return (
-    <div className="container mx-auto max-w-4xl">
-      <DataTableDemo />
-    </div>
-  );
-}
