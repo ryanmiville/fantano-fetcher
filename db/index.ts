38ee1562cd58015b27e9f3afdf493dd4b1a57dd0
Ryan Miville <ryan.miville@terminus.com>
2025-09-02 16:35:29 -0400

    clean slate

--- db/index.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
-const client = createClient({
-  url: process.env.DATABASE_URL as string,
-  authToken: process.env.DATABASE_AUTH_TOKEN,
-});
-export const db = drizzle(client);
-export { reviews } from "./schema";
